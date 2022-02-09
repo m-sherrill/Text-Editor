b9bd9fdd1e98a766fd13566bcf7c2363bd7d5044
Morgan Sherrill <mo.sherrill@outlook.com>
2022-02-09 00:41:27 -0800

    made a few changes and removed console logs

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -6,11 +6,11 @@ const initdb = async () => {
 console.log("IN INIT DB!!!")
   openDB('jate', 1, {
     upgrade(db) {
-      if (db.objectStoreNames.contains('jate')) {
+      if (db.objectStoreNames.contains('jate content')) {
         console.log('jate database already exists');
         return;
       }
-      db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
+      db.createObjectStore('jate content', { keyPath: 'id', autoIncrement: true });
       console.log('jate database created');
     },
   })
@@ -18,36 +18,24 @@ console.log("IN INIT DB!!!")
 
 // Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => {
-  try{
   console.log('Post to the database');
-  const jateDb  = await openDB('jate', 1);
-  console.log("after jatedb")
-  const tx = await jateDb.transaction('jate content', 'readwrite');
-  console.log("after tx")
-  const store = await tx.objectStore('jate content');
-  console.log("after store")
-  const request = await store.add({content});
-  console.log("after request")
-  const result = await request;
-  console.log('🚀 - data saved to the database', result.value);
-} catch(error) {
-  console.log("ERROR IN putDB!!")
-}
+  const jateDb  = await openDB('jate', 1)
+  const tx = jateDb.transaction('jate content', 'readwrite');
+  const store = tx.objectStore('jate content');
+  const request = store.add({content})
+  console.log('🚀 - data saved to the database');
+  return request
 }
 
 // Add logic for a method that gets all the content from the database
 export const getDb = async () => {
-try {
 const jateDb = await openDB('jate', 1);
  const tx = jateDb.transaction('jate content', 'readonly');
  const store = tx.objectStore('jate content');
  const request = store.getAll();
  const result = await request;
- console.log('result.value', result);
- return result.value;
-} catch(error) {
-  console.log(error, "ERROR IN GET DB!!!")
-}
+ console.log('🚀 - data received from to the database');
+ return result; 
 }
 
 
